612d9075b65821d3800bccd2282f508d286825d3
sidhi6 <sidhijain559@gmail.com>
2023-05-24 23:59:15 +0530

    fgfge

--- app/src/main/java/com/example/myapplication/sp_reg.java
+++ app/src/main/java/com/example/myapplication/sp_reg.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class sp_reg extends AppCompatActivity {
-
+    Button si;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sp_reg);
+        si=findViewById(R.id.button24);
+        si.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(sp_reg
+                        .this, sp_signin.class);
+                startActivity(intent);
+            }
+        });
+
     }
-}
+    }
+
