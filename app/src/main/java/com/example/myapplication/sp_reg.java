d217d453d1fa65c8baf7db026738860c7dac93c7
SIS301021 <scape.sids@gmail.com>
2023-05-24 15:56:12 +0530

    fst

---
+++ app/src/main/java/com/example/myapplication/sp_reg.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sp_reg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sp_reg);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/st_splist.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class st_splist extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_st_splist);
+    }
+}
